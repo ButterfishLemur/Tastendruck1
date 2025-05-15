a2b19cdaa0e853b71f87e4552326d9b67a0e9170
ButterfishLemur <butterfish369@gmail.com>
2025-05-15 10:19:47 +0200

    Add files via upload
    Nur Großbuchstaben Simuliert

--- Tastendruck1.cpp
+++ Tastendruck1.cpp
@@ -1,63 +1,82 @@
+//Tastendruck großbuchstaben
+
+
 #include <windows.h>
 #include <iostream>
 #include <cctype>
 using namespace std;
 
 
+
+
+
 void simulateKeyPress(BYTE keyCodeByte, char keyCodeChar) {
-    // Struktur für die Eingabe
-    INPUT ip;
+    // Struktur für die Eingabe in ip werden die Variablen von unten eingesetzt
+    INPUT ip[2] = { 0 };
 
-    // Eingabetyp auf Tastatur setzen
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.wScan = 0; // Kein Scan-Code
-    ip.ki.time = 0;
-    ip.ki.dwExtraInfo = 0;
+    // Allg. für eingabe sagt soviel wie: Eingabe ist Tastendruck 
+    ip[0].type = INPUT_KEYBOARD;
+    ip[0].ki.wVk = VK_SHIFT; // Virtueller Tastencode !Byte wird als wert erwartet
+    ip[0].ki.dwFlags = 0; // 0 für Tastendruck
 
-    // Taste drücken
-    ip.ki.wVk = keyCodeByte; // Virtueller Tastencode
-    ip.ki.dwFlags = 0; // 0 für Tastendruck
-    SendInput(1, &ip, sizeof(INPUT));
+
+        // Allg. für eingabe sagt soviel wie: Eingabe ist Tastendruck 
+    ip[1].type = INPUT_KEYBOARD;
+    ip[1].ki.wVk = keyCodeByte; // Virtueller Tastencode !Byte wird als wert erwartet
+    ip[1].ki.dwFlags = 0; // 0 für Tastendruck
+    
+    
+    SendInput(2, ip, sizeof(INPUT)); //Tastendruck wird gesendet
+
 
     // Taste loslassen
-    ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP für Tastenloslassen
-    SendInput(1, &ip, sizeof(INPUT));
+    ip[0].ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP für Tastenloslassen
+    ip[1].ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP für Tastenloslassen
+    SendInput(2, ip, sizeof(INPUT));
+    
 
     // Anzeigen, welcher Tastendruck simuliert wurde
-    std::cout << "Simulierter Tastendruck: " << char(tolower(keyCodeChar)) << std::endl;
+
+    BYTE scanCode = MapVirtualKey(keyCodeChar, MAPVK_VK_TO_VSC);
+    std::cout << "Simulierter Tastendruck: " << char(toupper(keyCodeChar)) << std::endl;
 
 }
 
 int main() {
-    
+
+
+
+    // EingabeVariable definieren
+
     char n;
-    
-    
+
+
 
     cin >> n;
     Sleep(2000);
 
-   
 
-    
-        BYTE keyCodeByte = toupper(n); // Wandelt den Buchstaben in Großbuchstaben um
+    //n wird in zwei unterschiedliche datentypen/ variablen gespeichert und an Simulatekeypress übergeben. 
+
+    BYTE keyCodeByte = toupper(n);
 
 
-        char keyCodeChar = toupper(n);
+    char keyCodeChar = toupper(n);
 
-    
- 
-   
-   
+
+
+
+
 
 
     // Überprüfen des virtuellen Tastencodes
-    UINT scanCode = MapVirtualKey(keyCodeByte, MAPVK_VK_TO_VSC);
-    std::cout << "Virtueller Tastencode: " << (int)keyCodeByte << ", Scan-Code: " << scanCode << std::endl;
     
+    
+
 
     simulateKeyPress(keyCodeByte, keyCodeChar);
 
     return 0;
 }
+
 
